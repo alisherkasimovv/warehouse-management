7bdc596aefad3e605ff9f8877f909b12382b36eb
Alisher Kasimov <alisherkasimov@gmail.com>
2020-09-01 12:46:47 +0500

    ServletInitializer refactoring;

--- src/main/java/uz/wh/Application.java
+++ src/main/java/uz/wh/Application.java
@@ -2,12 +2,22 @@ package uz.wh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	/*
+	 * Method helps to build application in correct way with compiling its classes
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 
 }
