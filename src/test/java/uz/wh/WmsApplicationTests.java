3306c1174d905e32ab3bbeb812d85002bd2d3e39
Alisher Kasimov <alisherkasimov@gmail.com>
2020-09-01 12:44:23 +0500

    Initial commit;

---
+++ src/test/java/uz/wh/WmsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.wh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
